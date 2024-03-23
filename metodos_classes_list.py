c1cd50d0d9ab236a4f28dbdafd99adf2c02eb750
Luiz Gustavo <111017865+Luizg961@users.noreply.github.com>
2024-03-23 17:32:19 -0300

    Add files via upload

---
+++ metodos_classes_list.py
@@ -0,0 +1,111 @@
+
+
+
+ # Filtrar lista
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+# Modificar valores
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero**2 for numero in numeros]
+print(quadrado)
+
+
+#apeend
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+#clear
+
+lista = [1, "Python", [40, 30, 20]]
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+lista.clear()
+
+print(lista)  # []
+
+#copy
+
+lista = [1, "Python", [40, 30, 20]]
+
+lista.copy()
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+#count
+cores = ["vermelho", "azul", "verde", "azul"]
+
+print(cores.count("vermelho"))  # 1
+print(cores.count("azul"))  # 2
+print(cores.count("verde"))  # 1
+
+#extend
+linguagens = ["python", "js", "c"]
+
+print(linguagens)  # ["python", "js", "c"]
+
+linguagens.extend(["java", "csharp"])
+
+print(linguagens)  # ["python", "js", "c", "java", "csharp"]
+
+#index
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.index("java"))  # 3
+print(linguagens.index("python"))  # 0
+
+#pop
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.pop())  # csharp
+print(linguagens.pop())  # java
+print(linguagens.pop())  # c
+print(linguagens.pop(0))  # python
+
+#remove
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.remove("c")
+
+print(linguagens)  # ["python", "js", "java", "csharp"]
+#reverse
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.reverse()
+
+print(linguagens)  # ["csharp", "java", "c", "js", "python"]
+
+#sort
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort()  # ["c", "csharp", "java", "js", "python"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(reverse=True)  # ["python", "js", "java", "csharp", "c"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x))  # ["c", "js", "java", "python", "csharp"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x), reverse=True)  # ["python", "csharp", "java", "js", "c"]
+print(linguagens)
+
+#len
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(len(linguagens))  # 5
+
+#sorted
+llinguagens = ["python", "js", "c", "java", "csharp"]
+
+print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
+print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
---
+++ trabalhando_listas_python.py
@@ -0,0 +1,48 @@
+
+#LISTAS
+
+frutas = ["laranja" , "limao", "maça" , "uva"]
+print(frutas)
+print(frutas[2])
+print(frutas[3])
+print(frutas[-2])
+print(frutas[-4])
+
+frutas = []
+print()
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["ferrari" , "f8" , 4200000 , 2020 , 2900 , "sao paulo" , True]
+print(carro)
+print()
+print()
+
+#MATRIZ
+matriz = [
+    [1,"A",3],
+    ["B",5,9],
+    ["#","L",8]
+]
+print(matriz[0])
+print(matriz[0][0])
+print(matriz[1] [2])
+print(matriz[2][0])
+print()
+print()
+
+#FATIAMENTO
+lista = ["p","y","t","h","o","n"]
+print(lista[2:])
+print(lista[:2])
+print(lista[1:3])
+print(lista[0:3:2])
+print(lista[::])
+print(lista[::-1])
+
+
+
